9af74fa3b7397940d5fc90285c2165e58dc3061f
sj2799 <snehajadhav2799@gmail.com>
2024-10-13 20:37:56 +0530

    Python comments and Escape sequence characters

---
+++ Comments.py
@@ -0,0 +1,16 @@
+a = 2 #Single line comments in python
+b = 6   
+
+'''This is 
+multi-line comments'''
+
+"""This is multi-line comments"""
+
+print (a+b)
+print ("\n")
+
+''' ESCAPE SEQUENCE CHARACTER '''
+"""To insert characters that cannot be directly used in a string"""
+#print ("This doesnot "execute")
+print ("This will \"execute")
+print ("This will \"execute\" ")
