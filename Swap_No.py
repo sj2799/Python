e572b02833036cc46b45a93e80c2bb2f5364fb2d
sj2799 <snehajadhav2799@gmail.com>
2024-10-14 17:14:49 +0530

    Swapping of 2 numbers by taking input from user added

--- Swap_No.py
+++ Swap_No.py
@@ -20,3 +20,14 @@ x,y = y,x
 print("The value of x after swapping:", x)
 print("The value of y after swapping:", y)
 
+#Swapping of 2 no's by taking input from user
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+x,y = y,x
+
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y)
+
+
+
