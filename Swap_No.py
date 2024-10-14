954f825f47c445953891af2776ac486f4a1e8cd7
sj2799 <snehajadhav2799@gmail.com>
2024-10-14 17:10:00 +0530

    Swapping of 2 numbers

---
+++ Swap_No.py
@@ -0,0 +1,22 @@
+#Swapping of 2 no's using temp variable
+
+x = 15
+y = 20
+
+temp = x
+x = y
+y = temp
+
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y)
+
+#Swapping of 2 no's without using temp variable
+
+x = 20
+y = 40
+
+x,y = y,x
+
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y)
+
